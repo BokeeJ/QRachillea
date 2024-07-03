73570ce0d0f7578e215ffc86fca6693605c4c711
BokeeJ <bokeejusthard@gmail.com>
2024-07-03 20:35:46 +0200

    repairedMenu

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,20 +1,25 @@
 
 import React from 'react'
+///react icons
+import { CiFacebook } from "react-icons/ci";
+import { CiInstagram } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+
 
 function Menu() {
     return (
         <div className='w-full lg:h-full h-screen flex justify-center relative bg-black'>
-            <img className='lg:w-[60%]  h-full  object-center' src="/Meni.webp" alt="meni" />
+            <img className='lg:w-[60%]  h-full  object-center' src="/AndraNew.webp" alt="meni" />
             {/* SocialNetwork */}
             <div className='absolute bottom-0 flex gap-3 lg:m-3 p-1'>
-                <a target='_blank' href="https://www.instagram.com/rakija_achillea?igsh=ZzVxY2dpMWUxeGlu" title="instagram">
-                    <img className='lg:w-7 lg:h-7 w-5 h-5' src="/instagram.png" alt="instagram" />
+                <a target='_blank' href="https://www.instagram.com/rakija_achillea/?igsh=ZzVxY2dpMWUxeGlu" title="instagram">
+                    <CiInstagram size={35} color='white' />
                 </a>
-                <a target='_blank' href="https://www.facebook.com/marko.a.andra/" title="instagram">
-                    <img className='lg:w-7 lg:h-7 w-5 h-5' src="/facebook.png" alt="facebook" />
+                <a target='_blank' href="https://www.facebook.com/people/Rakija-Achillea/100089827292627/" title="facebook">
+                    <CiFacebook size={35} color='white' />
                 </a>
                 <a target='_blank' href="https://www.linkedin.com/in/marko-andric/" title="instagram">
-                    <img className='lg:w-7 lg:h-7 w-5 h-5' src="/linkedin.png" alt="linkedin" />
+                    <CiLinkedin size={35} color='white' />
                 </a>
             </div>
             <div className='absolute bottom-0 right-0'>
